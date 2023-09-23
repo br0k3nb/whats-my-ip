47471b6bbc9db11c4de08521ceb4c4245b84676d
Rodrigo Oliveira <brokenb1312@hotmail.com>
2023-09-23 08:48:05 -0300

    adding the ip endpoints, removing node_modules (forgot to create .gitignore) and creating gitignore

--- routes.js
+++ routes.js
@@ -1,16 +1,40 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
-router.get('/json', (req, res) => {
-    var _a;
-    return res.status(200).json({ ip: (_a = res.socket) === null || _a === void 0 ? void 0 : _a.remoteAddress });
+import express from 'express';
+import { publicIpv4, publicIpv6 } from 'public-ip';
+const router = express.Router();
+router.get('/json', async (req, res) => {
+    try {
+        const ipv4 = await publicIpv4();
+        return res.status(200).json({ ip: ipv4 });
+    }
+    catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
-router.get('/', (req, res) => {
-    var _a;
-    return res.status(200).send((_a = res.socket) === null || _a === void 0 ? void 0 : _a.remoteAddress);
+router.get('/json/ipv6', async (req, res) => {
+    try {
+        const ipv6 = await publicIpv6();
+        return res.status(200).json({ ip: ipv6 });
+    }
+    catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
-exports.default = router;
+router.get('/', async (req, res) => {
+    try {
+        const ipv4 = await publicIpv4();
+        return res.status(200).json(ipv4);
+    }
+    catch (err) {
+        res.status(500).json({ message: err });
+    }
+});
+router.get('/ipv6', async (req, res) => {
+    try {
+        const ipv6 = await publicIpv6();
+        return res.status(200).json(ipv6);
+    }
+    catch (err) {
+        res.status(500).json({ message: err });
+    }
+});
+export default router;
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const routes_1 = __importDefault(require("./routes"));
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
-app.use((0, cors_1.default)());
-app.use(routes_1.default);
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(routes);
 app.listen(3001, () => console.log(`Server is running at port 3001`));
