f6fe8aaed292c80a2b1a341ac0b1bdc8508d2f01
Rodrigo Oliveira <brokenb1312@hotmail.com>
2023-09-23 10:44:12 -0300

    finishing very basic ip api

--- routes.ts
+++ routes.ts
@@ -1,23 +1,10 @@
-import express from 'express'
-import { v4, v6 } from 'public-ip';
+import express from 'express';
 
 const router = express.Router();
 
 router.get('/json', async (req, res) => {
     try {
-        const ipv4 = await v4();
-
-        return res.status(200).json({ ip: ipv4, test: req.ip });
-    } catch (err) {
-        res.status(500).json({ message: err });
-    }
-});
-
-router.get('/json/ipv6', async (req, res) => {
-    try {
-        const ipv6 = await v6();
-    
-        return res.status(200).json({ ip: ipv6 });
+        return res.status(200).json({ ip: req.ip });
     } catch (err) {
         res.status(500).json({ message: err });
     }
@@ -25,19 +12,7 @@ router.get('/json/ipv6', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
-        const ipv4 = await v4();
-    
-        return res.status(200).json(ipv4);
-    } catch (err) {
-        res.status(500).json({ message: err });
-    }
-});
-
-router.get('/ipv6', async (req, res) => {
-    try {
-        const ipv6 = await v6();
-
-        return res.status(200).json(ipv6);
+        return res.status(200).json(req.ip);
     } catch (err) {
         res.status(500).json({ message: err });
     }
