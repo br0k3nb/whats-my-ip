e4f13ad5e3ee709dc96477142e2bbd86ddcc9423
Rodrigo Oliveira <brokenb1312@hotmail.com>
2023-09-23 07:29:10 -0300

    starting the server

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(routes);
+
+app.listen(3001, () => console.log(`Server is running at port 3001`));
