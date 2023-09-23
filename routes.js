496139b5232e30936436c1179d016283067820e0
Rodrigo Oliveira <brokenb1312@hotmail.com>
2023-09-23 07:44:41 -0300

    returning the ip of the user

--- routes.js
+++ routes.js
@@ -5,7 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+router.get('/json', (req, res) => {
+    var _a;
+    return res.status(200).json({ ip: (_a = res.socket) === null || _a === void 0 ? void 0 : _a.remoteAddress });
+});
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+    var _a;
+    return res.status(200).send((_a = res.socket) === null || _a === void 0 ? void 0 : _a.remoteAddress);
 });
 exports.default = router;
--- routes.ts
+++ routes.ts
@@ -2,8 +2,12 @@ import express from 'express'
 
 const router = express.Router();
 
+router.get('/json', (req, res) => {
+    return res.status(200).json({ ip: res.socket?.remoteAddress });
+});
+
 router.get('/', (req, res) => {
-    res.send('Hello from vercel!');
+    return res.status(200).send(res.socket?.remoteAddress);
 });
 
 export default router;
